9c437ef0c8478df7dad4a9dde4aca8de39566d56
Muzaffer Karakuş <muzafferkarakus86@gmail.com>
2024-08-02 01:17:51 +0300

    Ders 6

---
+++ DataAccessLayer/Concrate/Context.cs
@@ -0,0 +1,16 @@
+﻿using EntityLayer.Concrate;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Concrate
+{
+    public class Context : IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=KARAKUS\\KARAKUS;initial catalog=EasyCashDb;integrated Security=true");
+        }
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+    }
+}
---
+++ DataAccessLayer/Migrations/20240801220631_mig_caP.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_caP : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Amount",
+                table: "CustomerAccountProcesses",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ProcessDate",
+                table: "CustomerAccountProcesses",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProcessType",
+                table: "CustomerAccountProcesses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "CustomerAccountProcesses");
+
+            migrationBuilder.DropColumn(
+                name: "ProcessDate",
+                table: "CustomerAccountProcesses");
+
+            migrationBuilder.DropColumn(
+                name: "ProcessType",
+                table: "CustomerAccountProcesses");
+        }
+    }
+}
